8255b9c45b839ed94ddd133f62ccdff402c2559c
CodeX047 <vishalrp047@gmail.com>
2026-05-03 21:08:27 +0530

    feat: initialize product listing interface with React, Vite, and Tailwind CSS

---
+++ React_Project/Product-Listing-Interface/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4 ">
+      <h1 className="text-3xl text-center">Product Listing</h1>
+      <hr />
+    </div>
+  );
+};
+
+export default App;
